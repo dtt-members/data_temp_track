bdeef6f98d095527d185a6ebe4ba12dbe559b65d
Pedro luís <pedro.passos@sptech.school>
2023-12-04 00:25:16 -0300

    caminho gráficos/ css site finalizado

--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -7,7 +7,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "DataBaseTrack",
     user: "root",
-    password: "Root#23#Sp",
+    password: "135790",
 };
 
 function executar(instrucao) {
