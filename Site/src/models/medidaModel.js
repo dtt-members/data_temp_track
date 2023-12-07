0c6ccc4de7646f1af7984220bf9818a9c68e9af6
guilhermesanttiago <guilherme.santiago@spetch.school>
2023-12-07 15:24:04 -0300

    Arrumei o erro nos gráficos

--- Site/src/models/medidaModel.js
+++ Site/src/models/medidaModel.js
@@ -2,11 +2,11 @@ var database = require("../database/config");
 
 function buscarUltimasMedidas(idAmb) {
         var instrucaoSql = `SELECT h.dataHist, h.dadoCap, s.tipoSensor, s.unidMedida FROM unidadedatacenter as dt JOIN ambiente as a ON a.fkDC = dt.idDataCenter JOIN sensor as s  ON s.fkAmb = a.idAmb LEFT JOIN hist as h ON h.fkSensor = s.idSensor
-        WHERE dt.fkEmp = ${idAmb} limit 5;`
+        WHERE dt.fkEmp = ${idAmb} ORDER by dataHist DESC limit 5;`
 
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         
-
+ 
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql)
 }
